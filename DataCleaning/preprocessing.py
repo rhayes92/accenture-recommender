8b89980e53e1bd06d48cb312720dfeb21b1f0601
haleyolsen11 <70980616+haleyolsen11@users.noreply.github.com>
2020-11-09 08:24:53 -0500

    Update and rename Pre-processing.py to preprocessing.py

--- DataCleaning/preprocessing.py
+++ DataCleaning/preprocessing.py
@@ -15,7 +15,6 @@ data_location = 's3://{}/{}'.format(bucket, data_key)
 df = pd.read_csv(data_location)
 
 # ### Data Preprocessing
-
 df.astype({"ORDER_TITLE":'str', 'LINE_DESCRIPTION': 'str'}) # changing data types so clean_text function works properly
 df.dropna(subset=['ORDER_TITLE'], how = 'any', inplace=True) # removed rows with null values in 'ORDER_TITLE' column 
 df.dropna(subset=['LINE_DESCRIPTION'], how = 'any', inplace=True) # removed rows with null values in 'LINE_DESCRIPTION' column
@@ -49,27 +48,27 @@ def clean_text(text):
     def remove_stopwords(text):
         return [word for word in text if word not in nltk.corpus.stopwords.words('english')]
         
-    def stemming(text):
-        ps = PorterStemmer()
-        return [ps.stem(word) for word in text]
+    #def stemming(text):
+        #ps = PorterStemmer()
+        #return [ps.stem(word) for word in text]
 
-    def lemmatize(text):
-        word_net = WordNetLemmatizer()
-        return [word_net.lemmatize(word) for word in text]
+    #def lemmatize(text):
+        #word_net = WordNetLemmatizer()
+        #return [word_net.lemmatize(word) for word in text]
 
     text = simplify_punctuation(text) # remove punctuation
     text = tokenize(text) # tokenize
     text = remove_stopwords(text) # remove stopwords
-    #text = stemming(text) # stemming
-    #text = lemmatize(text) # lemmatization, not used. Could use in combination with stemming but didn't think it was necessary. 
+    #text = stemming(text) # stemming, not used. Affected the ability for the word embedding layer to read words. 
+    #text = lemmatize(text) # lemmatization, not used. Affected the ability for the word embedding layer to read words. 
     return text
 
 
 # ### Apply Cleaning Function to Columns
 
-df['clean_title'] = df['ORDER_TITLE'].apply(lambda x : clean_text(x))
-df['clean_desc'] = df['LINE_DESCRIPTION'].apply(lambda x : clean_text(x))
-df['clean_title_desc'] = df['clean_title'] + df['clean_desc']
+df['clean_title'] = df['ORDER_TITLE'].apply(lambda x : clean_text(x)) # creating new column with the clean order title
+df['clean_desc'] = df['LINE_DESCRIPTION'].apply(lambda x : clean_text(x)) # creating new column with the clean line description
+df['clean_title_desc'] = df['clean_title'] + df['clean_desc'] # concatenating the clean order title and clean description to use as x value in the model
 df.head()
 
 ## removing tokens to return to sentence
@@ -80,7 +79,7 @@ df['clean_title'] = df['clean_title'].apply(lambda x : return_sentences(x))
 df['clean_desc'] = df['clean_desc'].apply(lambda x : return_sentences(x))
 df['clean_title_desc'] = df['clean_title'] + " " + df['clean_desc']
 
-# removing words with 3 or less characters
+# removing words with 3 or less characters - this improved the accuracy of the model
 df['clean_title'] = df['clean_title'].replace(r'\b\w{1,3}\b', "", regex=True)
 df['clean_desc'] = df['clean_desc'].replace(r'\b\w{1,3}\b', "", regex=True)
 df['clean_title_desc'] = df['clean_title_desc'].replace(r'\b\w{1,3}\b', "", regex=True)
